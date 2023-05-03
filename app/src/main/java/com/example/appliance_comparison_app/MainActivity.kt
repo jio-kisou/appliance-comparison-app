8a018f591bb44e04794f1f325ad20e3287b01c28
jio-kisou <jiotto.ppp.ntd@gmail.com>
2023-05-04 00:40:36 +0900

    git init

---
+++ app/src/main/java/com/example/appliance_comparison_app/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.appliance_comparison_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.appliance_comparison_app.ui.theme.AppliancecomparisonappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppliancecomparisonappTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AppliancecomparisonappTheme {
+        Greeting("Android")
+    }
+}
