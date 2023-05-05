8b800b16b894d55baee7592801655da580b4a974
jio-kisou <jiotto.ppp.ntd@gmail.com>
2023-05-05 23:22:59 +0900

    Add: JetPack Compose Study1

--- app/src/main/java/com/example/appliance_comparison_app/MainActivity.kt
+++ app/src/main/java/com/example/appliance_comparison_app/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.appliance_comparison_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.appliance_comparison_app.ui.theme.AppliancecomparisonappTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(stringResource(R.string.android))
                 }
             }
         }
@@ -31,7 +35,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column {
+        Text(text = "Hello $name!", color = Color.Red)
+        Text(text = "Hello $name!", color = Color.Blue, fontSize = 10.sp)
+        Text(text = "Hello $name!", color = Color(0xff77ddbb), fontSize = 30f.sp)
+    }
 }
 
 @Preview(showBackground = true)
